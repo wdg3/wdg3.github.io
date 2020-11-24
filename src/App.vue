1062f865e758501b941e81e4383fd5b1fd2df3a5
Will Geoghegan <wdg3@wills-mbp.lan>
2020-11-24 13:48:53 -0800

    mdb-vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+#app {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+}
+body {
+  background-color: #2e2e2e;
+}
+.text-poppy-red {
+  color: #ef5350 !important;
+}
+.btn-poppy-red {
+  background-color: #ef5350 !important;
+}
+.bg-poppy-red {
+  background-color: #ef5350 !important;
+}
+</style>
